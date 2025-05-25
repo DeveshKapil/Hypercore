930220d498e3ab08e5ef92148ead80969a0f3652
DeveshKapil <sushikapil0@gmail.com>
2025-05-25 16:54:20 +0530

    Code

---
+++ hypercore-gui/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/renderer/index.tsx',
+  target: 'electron-renderer',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-react',
+              '@babel/preset-typescript'
+            ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  output: {
+    filename: 'renderer.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}; 
